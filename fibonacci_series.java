974b3802ce80d9844c9c0d39e718c75886072527
Suraj1992001 <107202059+Suraj1992001@users.noreply.github.com>
2022-06-16 13:13:12 +0530

    Update fibonacci_series.java

--- fibonacci_series.java
+++ fibonacci_series.java
@@ -1,12 +1,21 @@
 import java.util.*;
 public class fibonacci_series 
 {
-	public static void main(String[] args) 
+	public static void main(String args[])
+	{
+		accept();
+	}	
+	public static void accep_details()
 	{
 		Scanner sc = new Scanner(System.in);
-		int n,next_num,first_num=0,sec_num=1;
+		int n;
 		System.out.println("Enter the number upto which you want to print fibonacci series:");
 		n = sc.nextInt();
+		fibonacci(n);
+	}	
+	public static void fibonacci(int n)
+	{
+		int next_num,first_num=0,sec_num=1;
 		while(first_num <= n)
 		{
 			System.out.print(first_num+" , ");
