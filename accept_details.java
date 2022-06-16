56c5095bb43bcfcc7487a0472f90060f72ebcf79
Suraj1992001 <107202059+Suraj1992001@users.noreply.github.com>
2022-06-16 23:02:07 +0530

    Update accept_details.java

--- accept_details.java
+++ accept_details.java
@@ -1,16 +1,15 @@
-/*WAP To Accept Student Name,Roll No. And Marks Of 5 Subjects
-And Then Calculate Percentage And Display The Output*/
-import java.util.*; //Importing Scanner Class
-public class accept_details //Name Of The Class
+//WAP To Accept Student Name,Roll No. And Marks Of 5 Subjects And Then Calculate Percentage And Display The Output
+import java.util.*;                             //Importing Scanner Class
+public class accept_details                     //Name Of The Class
 {
-    public static void main(String args[]) //Main Method
+    public static void main(String args[])      //Main Method
     {
-        String name; //Declaring name Variable
-        int roll_no,m1,m2,m3,m4,m5; //Declaring roll no. and marks of 5 subject variables
-        float percent; //Declaring percent Variable
-        Scanner sc = new Scanner(System.in); //Allow To Take Input
+        String name;                            //Declaring name Variable
+        int roll_no,m1,m2,m3,m4,m5;             //Declaring roll no. and marks of 5 subject variables
+        float percent;                          //Declaring percent Variable
+        Scanner sc = new Scanner(System.in);    //Allow To Take Input
         System.out.println("Enter Your Name:"); //Taking Input
-        name = sc.nextLine(); //Giving Input
+        name = sc.nextLine();                   //Giving Input
         System.out.println("Enter Your Roll No. :");
         roll_no = sc.nextInt();
         System.out.println("Enter Your Marks of 1st Subject :");
@@ -23,8 +22,8 @@ public class accept_details //Name Of The Class
         m4 = sc.nextInt();
         System.out.println("Enter Your Marks of 5th Subject :");
         m5 = sc.nextInt();
-        percent = ((m1+m2+m3+m4+m5)*100)/500F; //Calculating percentage
-        System.out.println("Name :"+name); //Displaying Output
+        percent = ((m1+m2+m3+m4+m5)*100)/500F;  //Calculating percentage
+        System.out.println("Name :"+name);      //Displaying Output
         System.out.println("Roll No. :"+roll_no);
         System.out.println("Marks of 1st Subject:"+m1);
         System.out.println("Marks of 2nd Subject:"+m2);
