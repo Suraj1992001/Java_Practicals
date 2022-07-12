d3dbcd1ec33c017b07ad7ee9115a668e9fbef6db
Suraj1992001 <107202059+Suraj1992001@users.noreply.github.com>
2022-07-12 13:19:14 +0530

    Add files via upload

---
+++ Abstraction/abs_demo.java
@@ -0,0 +1,24 @@
+package Abstraction;
+public class abs_demo extends abs_class 
+{
+	public static void main(String[] args)
+	{
+		abs_demo obj = new abs_demo();
+		obj.sod();
+		obj.fib();
+	}
+	@Override
+public void fib() 
+	{
+		int n,next_num,first_num=0,sec_num=1;
+		System.out.println("Enter the number upto which you want to print fibonacci series:");
+		n = sc.nextInt();
+		while(first_num < n)
+		{
+			System.out.print(first_num+" , ");
+			next_num = first_num + sec_num;
+			first_num = sec_num;
+			sec_num = next_num;
+		}
+	}
+}
