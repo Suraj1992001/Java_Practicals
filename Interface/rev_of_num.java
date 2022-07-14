c99f4fb990d4e7550208810979d3fbe3711601d1
Suraj1992001 <107202059+Suraj1992001@users.noreply.github.com>
2022-07-15 01:57:31 +0530

    Update rev_of_num.java

--- Interface/rev_of_num.java
+++ Interface/rev_of_num.java
@@ -1,5 +1,6 @@
-package Interface;
-public interface rev_of_num 
+//The Interface Which Is Going To Be Implemented In 'perform.java' Class
+package Interface;		//'Interface' Package Is Created So That It Can Hold Multiple Classes
+public interface rev_of_num 	//'rev_of_num' Interface Is Created
 {
-	public void rev();
+	public void rev();	//'rev' Function Is Created
 }
