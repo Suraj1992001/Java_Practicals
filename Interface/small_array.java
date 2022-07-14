db93b52713a04fa78158ec760e2812be493f7841
Suraj1992001 <107202059+Suraj1992001@users.noreply.github.com>
2022-07-15 02:00:53 +0530

    Update small_array.java

--- Interface/small_array.java
+++ Interface/small_array.java
@@ -1,5 +1,6 @@
-package Interface;
-public interface small_array 
+//The Interface Which Is Going To Be Implemented In 'perform.java' Class
+package Interface;				//'Interface' Package Is Created So That It Can Hold Multiple Classes
+public interface small_array 			//'small_array' Interface Is Created
 {
-	public void smallest_using_array();
+	public void smallest_using_array();	//'smallest_using_array()' Function Is Created
 }
