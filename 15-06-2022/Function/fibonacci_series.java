df45c2fe807aec890c02ce15dd665d8f72c59ec3
Suraj1992001 <107202059+Suraj1992001@users.noreply.github.com>
2022-07-15 01:17:21 +0530

    Update fibonacci_series.java

--- 15-06-2022/Function/fibonacci_series.java
+++ 15-06-2022/Function/fibonacci_series.java
@@ -1,16 +1,26 @@
-import java.util.*;
-public class fibonacci_series 
+//WAP To Accept A Number From User And Display The Fibonacci Seies
+import java.util.*;											//Importing Scanner Class
+public class fibonacci_series 										//Fibonacci Series Class
 {
-	public static void main(String[] args) 
+	public static void main(String args[])								//Main Class
 	{
-		Scanner sc = new Scanner(System.in);
-		int n,next_num,first_num=0,sec_num=1;
-		System.out.println("Enter the number upto which you want to print fibonacci series:");
-		n = sc.nextInt();
-		while(first_num <= n)
+		accept_details();									//Calling 'accept_details' Function
+	}	
+	public static void accept_details()								//'accept_details' Function
+	{
+		Scanner sc = new Scanner(System.in);							//Creating Object For Scanner Class
+		int n;											//Declaring Variable
+		System.out.println("Enter the number upto which you want to print fibonacci series:");	//Taking Input
+		n = sc.nextInt();									//Giving Input
+		fibonacci(n);										//Calling 'fibonacci' Function
+	}	
+	public static void fibonacci(int n)								//'fibonacci' Function
+	{
+		int next_num,first_num=0,sec_num=1;
+		while(first_num <= n)									//Using while loop
 		{
 			System.out.print(first_num+" , ");
-			next_num = first_num + sec_num;
+			next_num = first_num + sec_num;							//Performing Operations
 			first_num = sec_num;
 			sec_num = next_num;
 		}
