69cde6077abbf16e7eca9fb74aa2c1dd2f90f38c
Suraj1992001 <107202059+Suraj1992001@users.noreply.github.com>
2022-07-21 12:59:21 +0530

    Add files via upload

---
+++ 21-07-2022/Practicals/base_class_1.java
@@ -0,0 +1,22 @@
+package practicals_21_07_2022;
+
+public class base_class_1 
+{
+	private int Total,Pin;
+	public void set_Total(int newval)
+	{
+		Total=newval;
+	}
+	public int get_Total()
+	{
+		return Total;
+	}
+	public void set_Pin(int newval)
+	{
+		Pin=9654;
+	}
+	public int get_Pin()
+	{
+		return Pin;
+	}
+}
