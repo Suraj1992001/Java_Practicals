c424a00f4e20f8d9d3c7f283b3c883fe0f25e294
Suraj1992001 <107202059+Suraj1992001@users.noreply.github.com>
2022-07-21 13:33:39 +0530

    Update base_class_1.java

--- 21-07-2022/Practicals/base_class_1.java
+++ 21-07-2022/Practicals/base_class_1.java
@@ -1,15 +1,16 @@
-package practicals_21_07_2022;
-
-public class base_class_1 
+/*WAP To Accept Candidate Name, Bank Name, Phone Number, Account Number From User And By Encapsulation Make Balance And Pin Private 
+And Take Default Balance=500 And Take Credit Amount And Debit Amount From User And Display The Details If The Pin Is Correct*/
+package practicals_21_07_2022;			//Creating A Package
+public class base_class_1 			//Creating Super Class
 {
-	private int Total,Pin;
-	public void set_Total(int newval)
+	private int Total,Pin;			//Variable Declaration Privately
+	public void set_Total(int newval)	//'set' Method
 	{
-		Total=newval;
+		Total=newval;			//Assigning A Value To A Variable
 	}
-	public int get_Total()
+	public int get_Total()			//'get' Method
 	{
-		return Total;
+		return Total;			//Return Value
 	}
 	public void set_Pin(int newval)
 	{
